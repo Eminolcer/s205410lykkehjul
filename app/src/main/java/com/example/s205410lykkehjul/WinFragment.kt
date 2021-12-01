eff30895e8d0d0b98eab8b15451c2d8993283c21
Eminolcer <72021999+Eminolcer@users.noreply.github.com>
2021-12-01 18:27:32 +0100

    tilføjet spil igen funktion i win fragment

--- app/src/main/java/com/example/s205410lykkehjul/WinFragment.kt
+++ app/src/main/java/com/example/s205410lykkehjul/WinFragment.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import androidx.navigation.Navigation
 
 
 class WinFragment : Fragment() {
@@ -17,6 +18,16 @@ class WinFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_win, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val playAgain = view.findViewById<Button>(R.id.winButton)
+
+        playAgain.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.playAgainGoBack)
+        }
     }
 
 }
